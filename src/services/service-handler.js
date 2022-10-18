06290aa356d0c714721b52a0bc4fcda745cbdc29
Tholut Akhyar <tholut.akhyar@ai-ecom.io>
2022-10-18 22:01:23 +0700

    Update method from metadata to product type

--- src/services/service-handler.js
+++ src/services/service-handler.js
@@ -1,36 +1,40 @@
 import { BaseService } from "medusa-interfaces";
+import { getConfigFile } from "medusa-core-utils";
 
 class ServiceHandlerService extends BaseService {
-    constructor({ productService, shippingProfileService }) {
+    constructor({ productService, productRepository, shippingProfileService }, options) {
         super();
         this.productService = productService;
         this.shippingProfileService = shippingProfileService;
+        this.productRepository = productRepository;
+        this.typeID = options.serviceId || "";
+        this.typeName = options.serviceName || "Service";
+        this.defaultSelection = ["created_at", "updated_at", "deleted_at", "title", "type", "type_id", "id", "subtitle", "description", "handle", "metadata"];
+        this.defaultRelation = ["type"];
+        this.options = options;
     }
 
     //TODO Create new product variant which is a service
     async create(req, res) {
         const { title, handle } = req.body;
         const metadata = {
-            isService: true,
-            product: []
+            products: []
         }
 
         const entityManager = req.scope.resolve("manager")
-
         const newProduct = await entityManager.transaction(async (manager) => {
-
-        let shippingProfile = await this.shippingProfileService
-            .withTransaction(manager)
-            .retrieveDefault()
-    
-        const newProduct = await this.productService
-            .withTransaction(manager)
-            .create({ title: title, handle: handle, metadata: metadata, profile_id: shippingProfile.id })
-            return newProduct
+            let shippingProfile = await this.shippingProfileService
+                .withTransaction(manager)
+                .retrieveDefault()
+        
+            const newProduct = await this.productService
+                .withTransaction(manager)
+                .create({ title: title, handle: handle, metadata: metadata, profile_id: shippingProfile.id, type: { value: this.typeName } })
+                return newProduct
         })
     
         const product = await this.productService.retrieve(newProduct.id, {
-            select: ["created_at", "updated_at", "deleted_at", "title", "subtitle", "description", "handle"],
+            relations: this.defaultRelation
         });
         
         return product;
@@ -57,7 +61,7 @@ class ServiceHandlerService extends BaseService {
         }
 
         if (productIdFilteredList.length != productIdList.length) {
-            await this.productService.update(service_id, { metadata: { product: productIdFilteredList } });
+            await this.productService.update(service_id, { metadata: { products: productIdFilteredList } });
         }
         
         if (detailProduct) return productDetailList;
@@ -66,63 +70,50 @@ class ServiceHandlerService extends BaseService {
 
     //TODO Get all products which are services
     async list(req, res) {
-        const [products] = await this.productService.listAndCount({}, {});
-        const listService = [];
-        for (const product of products) {
-            if (product.metadata) {
-                if (product.metadata.isService) {
-                    const productList = await this.filteringExistProduct(product.id, product.metadata.product, true);
+        // Need more work for better find Type
+        const [products] = await this.productService.listAndCount({ type_id: this.typeID }, { relations: this.defaultRelation, select: this.defaultSelection });
 
-                    listService.push({
-                        id: product.id,
-                        created_at: product.created_at,
-                        updated_at: product.updated_at,
-                        deleted_at: product.deleted_at,
-                        title: product.title,
-                        subtitle: product.subtitle,
-                        description: product.description,
-                        handle: product.handle,
-                        status: product.status,
-                        product: productList
-                    });
-                }
+        for (const product of products) {
+            if (product.metadata?.products.length > 0) {
+                const productList = await this.filteringExistProduct(product.id, product.metadata.products, true);
+                product.products = productList;
             }
+            product.type = undefined;
+            product.metadata = undefined;
+            product.type_id = undefined;
         }
-        return listService;
+
+        return products;
     }
 
     async get(req, res) {
         const { id } = req.params;
-        const [products] = await this.productService.listAndCount({ id: id }, {});
-        for (const product of products) {
-            if (product.metadata) {
-                if (product.metadata.isService) {
-                    const productList = await this.filteringExistProduct(product.id, product.metadata.product, true);
+        const [products] = await this.productService.listAndCount({ id: id }, { select: this.defaultSelection });
 
-                    return {
-                        id: product.id,
-                        created_at: product.created_at,
-                        updated_at: product.updated_at,
-                        deleted_at: product.deleted_at,
-                        title: product.title,
-                        subtitle: product.subtitle,
-                        description: product.description,
-                        handle: product.handle,
-                        status: product.status,
-                        product: productList
-                    };
-                }
+        for (const product of products) {
+            if (product.metadata?.products.length > 0) {
+                const productList = await this.filteringExistProduct(product.id, product.metadata.products, true);
+                product.products = productList;
             }
+            product.type = undefined;
+            product.metadata = undefined;
+            product.type_id = undefined;
+
+            return product;
         }
+
         return {
-            error: "service not found :("
+            message: "service id not found :("
         }
     }
 
     //TODO Update service based on specific id
     async update(req, res) {
-        const { id, product } = req.body;
+        const { id, products } = req.body;
         
+        const serviceData = await this.productService.retrieve(id, { relations: this.defaultRelation });
+        if (serviceData.type == null || serviceData.type?.value != this.typeName) return { message: "sorry this item is not service :(" }
+
         const fieldList = [
             "title",
             "subtitle",
@@ -131,6 +122,7 @@ class ServiceHandlerService extends BaseService {
             "description",
             "subtitle"
         ];
+
         let updateDataQuery = {}
 
         for (const field of fieldList) {
@@ -142,11 +134,11 @@ class ServiceHandlerService extends BaseService {
             }
         }
 
-        if (product) {
-            const productList = await this.filteringExistProduct(id, product, false);
+        if (products) {
+            const productList = await this.filteringExistProduct(id, products, false);
             updateDataQuery = {
                 metadata: {
-                    product: productList
+                    products: productList
                 },
                 ...updateDataQuery
             }
@@ -164,13 +156,16 @@ class ServiceHandlerService extends BaseService {
             description: updateProduct.description,
             handle: updateProduct.handle,
             status: updateProduct.status,
-            product: updateProduct.metadata.product
+            products: updateProduct.metadata.products
         };
     }
 
     //TODO Delete service based on specific id
     async delete(req, res) {
         const { id } = req.params;
+        const serviceData = await this.productService.retrieve(id, { relations: this.defaultRelation });
+        if (serviceData.type == null || serviceData.type?.value != this.typeName) return { message: "sorry this item is not service :(" }
+        
         return await this.productService.delete(id);
     }
 }

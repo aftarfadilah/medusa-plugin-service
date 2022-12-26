3f267e4faf2abffc91840ce2c923728b20dd2e5c
akramaskar <akramaskar@gmail.com>
2022-12-26 21:24:57 +0100

    Small changes in the appointment admin route

--- src/api/routes/admin/appointments/get-appointment.ts
+++ src/api/routes/admin/appointments/get-appointment.ts
@@ -1,10 +1,14 @@
-import AppointmentService from "../../../../services/appointment"
+import AppointmentService from "../../../../services/appointment";
 
 export default async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
+  console.log("Req query", req.query);
 
-    const appointmentService: AppointmentService = req.scope.resolve("appointmentService")
-    const appointment = await appointmentService.retrieve(id, { relations: ["order"] })
+  const appointmentService: AppointmentService =
+    req.scope.resolve("appointmentService");
+  const appointment = await appointmentService.retrieve(id, {
+    relations: ["order"],
+  });
 
-    res.status(200).json({ appointment })
-}
+  res.status(200).json({ appointment });
+};
--- src/api/routes/admin/appointments/list-appointment.ts
+++ src/api/routes/admin/appointments/list-appointment.ts
@@ -1,71 +1,80 @@
-import { validator } from "../../../../utils/validator"
-import AppointmentService from "../../../../services/appointment"
-import { selector } from "../../../../types/appointment"
-import { IsDate, IsOptional, IsString, IsNumber } from "class-validator"
-import { Type } from "class-transformer"
+import { validator } from "../../../../utils/validator";
+import AppointmentService from "../../../../services/appointment";
+import { selector } from "../../../../types/appointment";
+import { IsDate, IsOptional, IsString, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
 
 export default async (req, res) => {
-    const validated = await validator(AdminGetAppointmentsParams, req.query)
+  const validated = await validator(AdminGetAppointmentsParams, req.query);
 
-    const selector: selector = {}
+  const selector: selector = {};
 
-    if (validated.name) {
-        selector.name = validated.name
-    }
+  if (validated.name) {
+    selector.name = validated.name;
+  }
 
-    if (validated.order_id) {
-        selector.order_id = validated.order_id
-    }
+  if (validated.order_id) {
+    selector.order_id = validated.order_id;
+  }
 
-    if (validated.code) {
-        selector.code = validated.code
-    }
+  if (validated.code) {
+    selector.code = validated.code;
+  }
 
-    const appointmentService: AppointmentService = req.scope.resolve("appointmentService")
-    const [appointments, count] = await appointmentService.list(selector, {
-        relations: ["order"],
-        skip: validated.offset,
-        take: validated.limit
-    })
+  if (validated.from) {
+    selector.from = validated.from;
+  }
 
-    res.status(200).json({
-        appointments,
-        count: count,
-        limit: validated.limit,
-        offset: validated.offset
-    })
-}
+  if (validated.to) {
+    selector.to = validated.to;
+  }
+
+  const appointmentService: AppointmentService =
+    req.scope.resolve("appointmentService");
+  const [appointments, count] = await appointmentService.list(selector, {
+    relations: ["order", "order.customer"],
+    skip: validated.offset,
+    take: validated.limit,
+  });
+
+  res.status(200).json({
+    appointments,
+    count: count,
+    limit: validated.limit,
+    offset: validated.offset,
+  });
+};
 
 export class AdminGetAppointmentsParams {
-    @IsString()
-    @IsOptional()
-    name?: string
+  @IsString()
+  @IsOptional()
+  name?: string;
 
-    @IsString()
-    @IsOptional()
-    code?: string
+  @IsString()
+  @IsOptional()
+  code?: string;
 
-    @IsString()
-    @IsOptional()
-    order_id?: string
+  @IsString()
+  @IsOptional()
+  order_id?: string;
 
-    @IsDate()
-    @IsOptional()
-    @Type(() => Date)
-    from: Date
-  
-    @IsDate()
-    @IsOptional()
-    @Type(() => Date)
-    to: Date
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  from: Date;
 
-    @IsNumber()
-    @IsOptional()
-    @Type(() => Number)
-    limit = 50
-  
-    @IsNumber()
-    @IsOptional()
-    @Type(() => Number)
-    offset = 0
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  to: Date;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  limit = 50;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  offset = 0;
 }

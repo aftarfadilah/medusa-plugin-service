029669d15a1c363e5ebbca55fee7c39cc4818c7e
Tholut Akhyar <tholut.akhyar@ai-ecom.io>
2022-12-29 02:55:25 +0700

    Rewrite divideTimes code

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,8 +1,8 @@
 export function formatDate(date) {
     var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+        month = '' + (d.getUTCMonth() + 1),
+        day = '' + d.getUTCDate(),
+        year = d.getUTCFullYear();
   
     if (month.length < 2) 
         month = '0' + month;
@@ -22,25 +22,34 @@ export function subDay(date, day) {
     const d = new Date(date)
     d.setUTCDate(d.getUTCDate() - day)
     return new Date(d)
-  }
+}
   
 
-export function divideTimes(from, to, byMinutes: number = 5) {
-    const f = new Date(from)
-    const t = new Date(to)
+export function divideTimes(f: Date, t: Date, byMinutes: number = 5) {
     const minutes = (t.getTime() - f.getTime())/1000/60
     const result = []
-
-    // add 1 because the last time it's not looped
+    
     for (let i = 0; i < (minutes/byMinutes); i++) {
-        let hours = Math.floor(f.getUTCHours() + (byMinutes * i) / 60)
-        let minutes = (f.getUTCMinutes() + (byMinutes * i)) % 60
+        const dateNow = new Date(f)
+        dateNow.setUTCMinutes(f.getUTCMinutes() + (byMinutes * i))
+        const key = formatDate(dateNow)
+        if (!result[key]) result[key] = []
+        let hours: any = dateNow.getUTCHours()
+        let minutes: any = dateNow.getUTCMinutes()
         if (hours < 10) hours = `0${hours}`
         if (minutes < 10) minutes = `0${minutes}`
-        result.push(`${hours}:${minutes}`)
+        result[key].push(`${hours}:${minutes}`)
     }
 
     return result
+}
+
+export function countDays(from, to) {
+    const date1 = new Date(from);
+    const date2 = new Date(to);
+    const differenceInTime = date2.getTime() - date1.getTime();
+    const differenceInDays = differenceInTime / (1000 * 60 * 60 * 24);
+    return differenceInDays
 }
 
 export function zeroTimes(date) {

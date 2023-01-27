62ff7690d4f7eb9bca01196ffd60a4fd28c340a3
akramaskar <akramaskar@gmail.com>
2023-01-27 16:34:38 +0100

    Check if appointment is created

---
+++ dev_plugin_linking.sh
@@ -0,0 +1,7 @@
+rm -rf node_modules/medusa-interfaces
+rm -rf node_modules/@medusajs/medusa
+rm -rf node_modules/typeorm
+yarn link medusa-interfaces
+yarn link @medusajs/medusa
+yarn link typeorm
+yarn link
